924e759b1f4416b0706db16610b5d80d6729318a
muizbello <bellomuiz78+1@gmail.com>
2024-05-17 07:02:06 +0000

    formatArrayStrings function added

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -7,3 +7,22 @@ function processArray(testArray){
         }
     });
 };
+
+function formatArrayStrings(stringArray, processedArray){
+        newArray = [];
+        for(i = 0; i < stringArray.length; i++){
+            console.log(i);
+            if (processedArray[i] % 2 === 0){
+                newArray.push(stringArray[i].toUpperCase());
+            }else {
+                newArray.push(stringArray[i].toLowerCase());
+            }  
+        }
+         return( newArray);
+          
+};
+
+new0 = processArray([1,2,3]);
+console.log(new0);
+new1 = formatArrayStrings(["KWAKU", "Jonah","fireship"], new0);
+console.log(new1);
