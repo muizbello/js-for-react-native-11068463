4522574025c35d2bb1fa4e082368785a3e120dd4
muizbello <bellomuiz78+1@gmail.com>
2024-05-18 11:16:24 +0000

    Exporting functionality included.

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,5 +1,5 @@
 function processArray(testArray){
-    return testArray.map(value => {
+    return testArray.map((value) => {
         if (value % 2 === 0) {
             return value ** 2;
         } else {
@@ -12,7 +12,6 @@ function formatArrayStrings(stringArray, processedArray){
         newArray = [];
         if(stringArray.length === processedArray.length){
         for(i = 0; i < stringArray.length; i++){
-            console.log(i);
             if (processedArray[i] % 2 === 0){
                 newArray.push(stringArray[i].toUpperCase());
             }else {
@@ -26,6 +25,5 @@ function formatArrayStrings(stringArray, processedArray){
        
 };
 
-new0 = processArray([1, 3, 4]);
-new1 = formatArrayStrings(['kwaku','jonah','fireship'],new0);
-console.log(new1);
+module.exports = {processArray, formatArrayStrings};
+
